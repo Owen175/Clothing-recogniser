9633875e884d0b785470070cd60739f372e4c772
Owen175 <88042260+Owen175@users.noreply.github.com>
2022-11-28 12:56:42 +0000

    Create Testing.py

---
+++ Testing.py
@@ -0,0 +1,17 @@
+predictions = model.predict(test_images)
+COLOR = 'white'
+plt.rcParams['text.color'] = COLOR
+plt.rcParams['axes.labelcolor'] = COLOR
+
+num = int(input("Pick a number : "))
+
+place = np.argmax(predictions[num])
+print("Expected: ", class_names[test_labels[num]])
+
+plt.figure()
+plt.imshow(test_images[num], cmap=plt.cm.binary)
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+print("Guess: ", class_names[place])
