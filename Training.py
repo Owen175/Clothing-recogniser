24feee89da158c86c7f7c03a05e0ccbe4c9d0e14
Owen175 <88042260+Owen175@users.noreply.github.com>
2022-11-28 16:13:39 +0000

    Update Training.py

--- Training.py
+++ Training.py
@@ -5,37 +5,38 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+class model():
+  def __init__(self):
+    self.fashion_mnist = keras.datasets.fashion_mnist  # load dataset
 
-fashion_mnist = keras.datasets.fashion_mnist  # load dataset
+    (self.train_images, self.train_labels), (self.test_images, self.test_labels) = self.fashion_mnist.load_data()  # split into testing and training data
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()  # split into testing and training data
-
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-               
-              
-              
-train_images = train_images / 255.0
-
-test_images = test_images / 255.0
-
-#this makes it more compatible for the AI - the weights and biases will be closer to 0 or 1 which is what the thing scaled it down for
+    self.class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 
-#Passes through the layers sequentially
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),  # input layer (1): This flattens the 2d array to a 1d array
-    keras.layers.Dense(128, activation='relu'),  # hidden layer (2): all the neurones in previous layer connect to all in this layer - dense
-    keras.layers.Dense(10, activation='softmax') # output layer (3)
-])                                    #Softmax makes everything between 0 and one and makes them sum to one. activation functions. Outputs 10 different outcomes
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-#sets the algorithms
-#This is called hyperperameter tuning - when you change the parameters
+    self.train_images = self.train_images / 255.0
 
-model.fit(train_images, train_labels, epochs=1)  # we pass the data, labels and epochs
-#Fits the model to the training data. No input function because keras does it
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=1) 
-print('Test accuracy:', test_acc)
+    self.test_images = self.test_images / 255.0
+
+    #this makes it more compatible for the AI - the weights and biases will be closer to 0 or 1 which is what the thing scaled it down for
+
+
+    #Passes through the layers sequentially
+    eslf.model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),  # input layer (1): This flattens the 2d array to a 1d array
+        keras.layers.Dense(128, activation='relu'),  # hidden layer (2): all the neurones in previous layer connect to all in this layer - dense
+        keras.layers.Dense(10, activation='softmax') # output layer (3)
+    ])                                    #Softmax makes everything between 0 and one and makes them sum to one. activation functions. Outputs 10 different outcomes
+
+    self.model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    #sets the algorithms
+    #This is called hyperperameter tuning - when you change the parameters
+
+    model.fit(train_images, train_labels, epochs=1)  # we pass the data, labels and epochs
+    #Fits the model to the training data. No input function because keras does it
+    test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=1) 
+    print('Test accuracy:', test_acc)
